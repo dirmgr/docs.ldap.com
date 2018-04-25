e6619378195853160dc3c8df587b9cd6c38b2b34
Neil A. Wilson <nawilson@nawilson.com>
2018-04-25 02:20:29 -0500

    Snapshot of docs.ldap.com as of 2018-04-25

---
+++ www/ldap-sdk/LDAPCon-2009-presentation/index.php
@@ -0,0 +1,201 @@
+<?php $thisPage="products"; ?>
+<?php include('../../../inc/dochead_start.php') ?>
+
+<title>UnboundID &ndash; Products &ndash; Identity Data Store, Identity Proxy, Identity Data Sync, LDAP SDK for Java, Server SDK</title>
+
+<meta name="description" content="UnboundID provides an identity data platform for cloud service providers, telecommunication providers and enterprises through its Identity Data Store, Identity Proxy, and Identity Data Sync products." />
+<meta name="keywords" content="UnboundID, Identity Economy, Identity Data Platform, Identity Data Store, Identity Proxy, Identity Data Sync, Directory, Directory Services, Directory Server, Directory Proxy, LDAP, LDAP Proxy, LDAP SDK, Java, X.500, Subscriber Data Management, DSEE, Directory Server Enterprise Edition, Apertio, Identity Data, Personal Data, PII, Identity Management" />
+
+<?php include('../../../inc/dochead_end.php') ?>
+
+<?php include('../../_subnavs.php') ?>
+<?php $subnavs[5]['active'] = 1; ?>
+
+<?php include('../../../inc/topnav.php') ?>
+<?php include('../../../inc/subnav.php') ?>
+
+<div id="subhdr">
+  <div class="container-wrapper">
+    <div class="right-col-full">
+      <h1>LDAPCon 2009 Presentation</h1>
+    </div>
+    <div class="clear">
+    </div>
+  </div>
+</div>
+
+<div class="container-wrapper paddingB30">
+  <div class="left-container">
+    <h2>LDAP Development Made Simple</h2>
+    
+     <p>We attended LDAPCon 2009 in Portland, Oregon on September 20 and 21 and gave a presentation discussing how the UnboundID LDAP SDK for Java may be used to simplify the process for developing directory-enabled applications.  Slides for that presentation are <a href="LDAP-Development-Made-Simple-HandoutVersion.pdf">available here</a>, but we have also recorded a version of the presentation.  That recording is available below.  You must have a flash player installed to view this recording.</p>
+
+<p>
+  Part 1:  Introduction (Runtime:  3:54)
+  <br>
+  <object id="csSWF" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="640" height="535"
+          codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version==8">
+    <param name="src" value="LDAPCon-Recorded-Presentation-1-Introduction_controller.swf#" />
+    <param name="bgcolor" value="000000" />
+    <param name="quality" value="best" />
+    <param name="flashVars"
+           value="csConfigFile=LDAPCon-Recorded-Presentation-1-Introduction_config.xml&csColor=FFFFFF" />
+    <embed name="csSWF" src="LDAPCon-Recorded-Presentation-1-Introduction_controller.swf" width="640"
+           height="535" bgcolor="FFFFFF" quality="best" allowScriptAccess="always"
+           flashVars="csConfigFile=LDAPCon-Recorded-Presentation-1-Introduction_config.xml&csColor=FFFFFF"
+           pluginspage="http://www.adobe.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash">
+    </embed>
+  </object>
+</p>
+
+<p>
+  Part 2:  About the LDAP SDK (Runtime:  5:10)
+  <br>
+  <object id="csSWF" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="640" height="535"
+          codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version==8">
+    <param name="src" value="LDAPCon-Recorded-Presentation-2-About-the-LDAP-SDK_controller.swf#" />
+    <param name="bgcolor" value="000000" />
+    <param name="quality" value="best" />
+    <param name="flashVars"
+           value="csConfigFile=LDAPCon-Recorded-Presentation-2-About-the-LDAP-SDK_config.xml&csColor=FFFFFF" />
+    <embed name="csSWF" src="LDAPCon-Recorded-Presentation-2-About-the-LDAP-SDK_controller.swf" width="640"
+           height="535" bgcolor="FFFFFF" quality="best" allowScriptAccess="always"
+           flashVars="csConfigFile=LDAPCon-Recorded-Presentation-2-About-the-LDAP-SDK_config.xml&csColor=FFFFFF"
+           pluginspage="http://www.adobe.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash">
+    </embed>
+  </object>
+</p>
+
+<p>
+  Part 3:  LDAP SDK Code Comparisons (Runtime:  5:50)
+  <br>
+  <object id="csSWF" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="640" height="535"
+          codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version==8">
+    <param name="src" value="LDAPCon-Recorded-Presentation-3-SDK-Code-Comparisons_controller.swf#" />
+    <param name="bgcolor" value="000000" />
+    <param name="quality" value="best" />
+    <param name="flashVars"
+           value="csConfigFile=LDAPCon-Recorded-Presentation-3-SDK-Code-Comparisons_config.xml&csColor=FFFFFF" />
+    <embed name="csSWF" src="LDAPCon-Recorded-Presentation-3-SDK-Code-Comparisons_controller.swf" width="640"
+           height="535" bgcolor="FFFFFF" quality="best" allowScriptAccess="always"
+           flashVars="csConfigFile=LDAPCon-Recorded-Presentation-3-SDK-Code-Comparisons_config.xml&csColor=FFFFFF"
+           pluginspage="http://www.adobe.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash">
+    </embed>
+  </object>
+</p>
+
+<p>
+  Part 4:  Developer Friendliness (Runtime:  7:47)
+  <br>
+  <object id="csSWF" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="640" height="535"
+          codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version==8">
+    <param name="src" value="LDAPCon-Recorded-Presentation-4-Developer-Friendliness_controller.swf#" />
+    <param name="bgcolor" value="000000" />
+    <param name="quality" value="best" />
+    <param name="flashVars"
+           value="csConfigFile=LDAPCon-Recorded-Presentation-4-Developer-Friendliness_config.xml&csColor=FFFFFF" />
+    <embed name="csSWF" src="LDAPCon-Recorded-Presentation-4-Developer-Friendliness_controller.swf" width="640"
+           height="535" bgcolor="FFFFFF" quality="best" allowScriptAccess="always"
+           flashVars="csConfigFile=LDAPCon-Recorded-Presentation-4-Developer-Friendliness_config.xml&csColor=FFFFFF"
+           pluginspage="http://www.adobe.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash">
+    </embed>
+  </object>
+</p>
+
+<p>
+  Part 5:  Android Platform Demo (Runtime:  2:28)
+  <br>
+  <object id="csSWF" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="640" height="535"
+          codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version==8">
+    <param name="src" value="LDAPCon-Recorded-Presentation-5-Android-Demo_controller.swf#" />
+    <param name="bgcolor" value="000000" />
+    <param name="quality" value="best" />
+    <param name="flashVars"
+           value="csConfigFile=LDAPCon-Recorded-Presentation-5-Android-Demo_config.xml&csColor=FFFFFF" />
+    <embed name="csSWF" src="LDAPCon-Recorded-Presentation-5-Android-Demo_controller.swf" width="640"
+           height="535" bgcolor="FFFFFF" quality="best" allowScriptAccess="always"
+           flashVars="csConfigFile=LDAPCon-Recorded-Presentation-5-Android-Demo_config.xml&csColor=FFFFFF"
+           pluginspage="http://www.adobe.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash">
+    </embed>
+  </object>
+</p>
+
+<p>
+  Part 6:  Performance and Tools (Runtime:  8:53)
+  <br>
+  <object id="csSWF" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="640" height="535"
+          codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version==8">
+    <param name="src" value="LDAPCon-Recorded-Presentation-6-Performance-and-Tools_controller.swf#" />
+    <param name="bgcolor" value="000000" />
+    <param name="quality" value="best" />
+    <param name="flashVars"
+           value="csConfigFile=LDAPCon-Recorded-Presentation-6-Performance-and-Tools_config.xml&csColor=FFFFFF" />
+    <embed name="csSWF" src="LDAPCon-Recorded-Presentation-6-Performance-and-Tools_controller.swf" width="640"
+           height="535" bgcolor="FFFFFF" quality="best" allowScriptAccess="always"
+           flashVars="csConfigFile=LDAPCon-Recorded-Presentation-6-Performance-and-Tools_config.xml&csColor=FFFFFF"
+           pluginspage="http://www.adobe.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash">
+    </embed>
+  </object>
+</p>
+
+<p>
+  Part 7:  Migration and Code Metrics (Runtime:  5:22)
+  <br>
+  <object id="csSWF" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="640" height="535"
+          codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version==8">
+    <param name="src" value="LDAPCon-Recorded-Presentation-7-Migration-and-Code-Metrics_controller.swf#" />
+    <param name="bgcolor" value="000000" />
+    <param name="quality" value="best" />
+    <param name="flashVars"
+           value="csConfigFile=LDAPCon-Recorded-Presentation-7-Migration-and-Code-Metrics_config.xml&csColor=FFFFFF" />
+    <embed name="csSWF" src="LDAPCon-Recorded-Presentation-7-Migration-and-Code-Metrics_controller.swf" width="640"
+           height="535" bgcolor="FFFFFF" quality="best" allowScriptAccess="always"
+           flashVars="csConfigFile=LDAPCon-Recorded-Presentation-7-Migration-and-Code-Metrics_config.xml&csColor=FFFFFF"
+           pluginspage="http://www.adobe.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash">
+    </embed>
+  </object>
+</p>
+
+<p>
+  Part 8:  Future Plans (Runtime:  3:39)
+  <br>
+  <object id="csSWF" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="640" height="535"
+          codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version==8">
+    <param name="src" value="LDAPCon-Recorded-Presentation-8-Future-Plans_controller.swf#" />
+    <param name="bgcolor" value="000000" />
+    <param name="quality" value="best" />
+    <param name="flashVars"
+           value="csConfigFile=LDAPCon-Recorded-Presentation-8-Future-Plans_config.xml&csColor=FFFFFF" />
+    <embed name="csSWF" src="LDAPCon-Recorded-Presentation-8-Future-Plans_controller.swf" width="640"
+           height="535" bgcolor="FFFFFF" quality="best" allowScriptAccess="always"
+           flashVars="csConfigFile=LDAPCon-Recorded-Presentation-8-Future-Plans_config.xml&csColor=FFFFFF"
+           pluginspage="http://www.adobe.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash">
+    </embed>
+  </object>
+</p>
+
+<p>
+  Part 9:  Conclusion (Runtime:  2:02)
+  <br>
+    <object id="csSWF" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="640" height="535"
+          codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version==8">
+    <param name="src" value="LDAPCon-Recorded-Presentation-9-Conclusion_controller.swf#" />
+    <param name="bgcolor" value="000000" />
+    <param name="quality" value="best" />
+    <param name="flashVars"
+           value="csConfigFile=LDAPCon-Recorded-Presentation-9-Conclusion_config.xml&csColor=FFFFFF" />
+    <embed name="csSWF" src="LDAPCon-Recorded-Presentation-9-Conclusion_controller.swf" width="640"
+           height="535" bgcolor="FFFFFF" quality="best" allowScriptAccess="always"
+           flashVars="csConfigFile=LDAPCon-Recorded-Presentation-9-Conclusion_config.xml&csColor=FFFFFF"
+           pluginspage="http://www.adobe.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash">
+    </embed>
+  </object>
+</p>
+
+</div>
+
+<?php include('../../../inc/rtcol_start.php') ?>
+<?php include('../../../inc/rtcol_next-steps.php') ?>
+<?php include('../../../inc/rtcol_feat-download-ldap-sdk.php') ?>
+<?php include('../../../inc/rtcol_end.php') ?>
+<?php include('../../../inc/footer.php') ?>
